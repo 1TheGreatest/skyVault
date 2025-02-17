93d4d0a625b8cf440104d734e12b570e20b126b7
1TheGreatest <soloeben12@gmail.com>
2025-02-17 23:39:57 +0000

    card dropdown feature added

--- components/ActionDropDown.tsx
+++ components/ActionDropDown.tsx
@@ -1,9 +1,80 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Image from "next/image";
+import { Models } from "node-appwrite";
+import { actionsDropdownItems } from "@/constants";
+import { constructDownloadUrl } from "@/lib/utils";
+import Link from "next/link";
 
-const ActionDropDown = () => {
+const ActionDropDown = ({ file }: { file: Models.Document }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [action, setAction] = useState<ActionType | null>(null);
+
   return (
-    <div>ActionDropDown</div>
-  )
-}
+    <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
+      <DropdownMenuTrigger className="shad-no-focus">
+        <Image src="/icons/dots.svg" alt="dots" width={34} height={34} />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel className="max-w-[200px] truncate">
+          {file.name}
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {actionsDropdownItems.map((actionItem) => (
+          <DropdownMenuItem
+            key={actionItem.value}
+            className="shad-dropdown-item"
+            onClick={() => {
+              setAction(actionItem);
 
-export default ActionDropDown
+              if (
+                ["rename", "share", "delete", "details"].includes(
+                  actionItem.value
+                )
+              ) {
+                setIsModalOpen(true);
+              }
+            }}
+          >
+            {actionItem.value === "download" ? (
+              <Link
+                href={constructDownloadUrl(file.bucketFileId)}
+                download={file.name}
+                className="flex items-center gap-2"
+              >
+                <Image
+                  src={actionItem.icon}
+                  alt={actionItem.label}
+                  width={30}
+                  height={30}
+                />
+                {actionItem.label}
+              </Link>
+            ) : (
+              <div className="flex items-center gap-2">
+                <Image
+                  src={actionItem.icon}
+                  alt={actionItem.label}
+                  width={30}
+                  height={30}
+                />
+                {actionItem.label}
+              </div>
+            )}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default ActionDropDown;
