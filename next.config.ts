4ebc5568b646b7ebeffce214d11683d96192732f
1TheGreatest <soloeben12@gmail.com>
2025-02-17 23:14:18 +0000

    file card added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "100MB", // Adjust the body size limit since nextjs default is 10MB */
+    },
+  },
   images: {
     remotePatterns: [
       {
