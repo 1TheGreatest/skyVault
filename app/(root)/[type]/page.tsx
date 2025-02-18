1afe246081328fdb1dddd7366fc6a28ebbd58b34
1TheGreatest <soloeben12@gmail.com>
2025-02-18 22:23:29 +0000

    search bar feature

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,13 +1,19 @@
 import Card from "@/components/Card";
 import Sort from "@/components/Sort";
 import { getFiles } from "@/lib/actions/file.action";
+import { getFileTypesParams } from "@/lib/utils";
 import { Models } from "node-appwrite";
 import React from "react";
 
-const Page = async ({ params }: SearchParamProps) => {
+const Page = async ({ searchParams, params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || "";
 
-  const files = await getFiles();
+  const searchText = ((await searchParams)?.query as string) || "";
+  const sort = ((await searchParams)?.sort as string) || "";
+
+  const types = getFileTypesParams(type) as FileType[];
+
+  const files = await getFiles({ types, searchText, sort });
   return (
     <div className="page-container">
       <section className="w-full">
@@ -30,7 +36,7 @@ const Page = async ({ params }: SearchParamProps) => {
         <section className="file-list">
           {/* Render each file here */}
           {files.documents.map((file: Models.Document) => (
-              <Card key={file.$id} file={file} />
+            <Card key={file.$id} file={file} />
           ))}
         </section>
       ) : (
