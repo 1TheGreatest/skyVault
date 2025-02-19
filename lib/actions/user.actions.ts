10ff9f3339b7e1a4df2f6b944b1804647d8d095e
1TheGreatest <soloeben12@gmail.com>
2025-02-19 01:20:54 +0000

    fix deployment

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -118,6 +118,7 @@ export const getCurrentUser = async () => {
     return parseStringify(user.documents[0]);
   } catch (error) {
     handleError(error, "Failed to get current user");
+    return null;
   }
 };
 
